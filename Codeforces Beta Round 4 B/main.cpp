a60dbb0c20534118105d90a20f96e1d40aa52e81
AdamBJ <anb1@sfu.ca>
2014-10-10 15:28:42 +0900

    first working version

--- Codeforces Beta Round 4 B/main.cpp
+++ Codeforces Beta Round 4 B/main.cpp
@@ -6,25 +6,59 @@ using std::vector;
 
 int main() {
 
-	int d,totalHours,min,max;
+	int numDays,totalHours,min,max;
 
-	scanf("%d %d", &d, &totalHours);
+	scanf("%d %d", &numDays, &totalHours);
 
 	// declare 2D vector
 	vector< vector<int> > dailyStudy;
 	vector<int> bookend{ -999 };
 	dailyStudy.push_back(bookend);
-	for (int i = 1; i <= d; i++){
+	for (int i = 1; i <= numDays; i++){
 		scanf("%d %d", &min, &max);
 		// make new entry for current day
 		vector<int> day{ min, max, max };
 		dailyStudy.push_back(day);
 	}
 
-	for (int i = 0; i < dailyStudy.size(); i++)
-	{
-		for (int q = 0; q < dailyStudy[i].size(); q++)
-		std::cout << dailyStudy[i][q] << "\n";
+	int currentTotal = 0, tempTotal, d = 1;
+	bool solnFound = false;
+	while (d != 0) {
+		//select path greedily
+		tempTotal = currentTotal + dailyStudy[d][1];
+
+		if (tempTotal > totalHours || tempTotal < totalHours && d == numDays){
+			if (dailyStudy[d][1] != dailyStudy[d][0]){
+				dailyStudy[d][1]--;
+			}
+			else if (dailyStudy[d][1] == dailyStudy[d][0]){
+				//reset
+				do{
+					dailyStudy[d][1] = dailyStudy[d][2];
+					d--;
+					if (d != 0 ) currentTotal -= dailyStudy[d][1];
+					else break;
+				} while (dailyStudy[d][1] == dailyStudy[d][0]);
+				if (d != 0) dailyStudy[d][1]--;
+			}
+		}
+		else if (tempTotal == totalHours){
+			solnFound = true;
+			break;
+		}
+		else {
+			d++;
+			currentTotal = tempTotal;
+		}
 	}
+
+	if (solnFound){
+		std::cout << "YES\n";
+		for (int i = 1; i < dailyStudy.size(); i++)
+		{
+			std::cout << dailyStudy[i][1] << " ";
+		}
+	} else std::cout << "NO";
+
 	return 0;
 }
